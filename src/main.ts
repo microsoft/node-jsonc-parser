c38aac4e7e9ce36146dbc33b4d35efdcb88a4ad2
Martin Aeschlimann <martinae@microsoft.com>
2016-04-18 22:40:50 +0200

    nls error messages

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
+import * as nls from 'vscode-nls';
+nls.config(process.env['VSCODE_NLS_CONFIG']);
+
+const localize = nls.loadMessageBundle();
+
 export enum ScanError {
 	None,
 	UnexpectedEndOfComment,
@@ -631,15 +636,15 @@ export enum ParseErrorCode {
 
 export function getParseErrorMessage(errorCode: ParseErrorCode) : string {
 	switch (errorCode) {
-		case ParseErrorCode.InvalidSymbol: return 'Invalid symbol';
-		case ParseErrorCode.InvalidNumberFormat: return 'Invalid number format';
-		case ParseErrorCode.PropertyNameExpected: return 'Property name expected';
-		case ParseErrorCode.ValueExpected: return 'Value expected';
-		case ParseErrorCode.ColonExpected: return 'Colon expected';
-		case ParseErrorCode.CommaExpected: return 'Comma expected';
-		case ParseErrorCode.CloseBraceExpected: return 'Closing brace expected';
-		case ParseErrorCode.CloseBracketExpected: return 'Closing bracket expected';
-		case ParseErrorCode.EndOfFileExpected: return 'End of file expected';
+		case ParseErrorCode.InvalidSymbol: return localize('error.invalidSymbol', 'Invalid symbol');
+		case ParseErrorCode.InvalidNumberFormat: return localize('error.invalidNumberFormat', 'Invalid number format');
+		case ParseErrorCode.PropertyNameExpected: return localize('error.propertyNameExpected', 'Property name expected');
+		case ParseErrorCode.ValueExpected: return localize('error.valueExpected', 'Value expected');
+		case ParseErrorCode.ColonExpected: return localize('error.colonExpected', 'Colon expected');
+		case ParseErrorCode.CommaExpected: return localize('error.commaExpected', 'Comma expected');
+		case ParseErrorCode.CloseBraceExpected: return localize('error.closeBraceExpected', 'Closing brace expected');
+		case ParseErrorCode.CloseBracketExpected: return localize('error.closeBracketExpected', 'Closing bracket expected');
+		case ParseErrorCode.EndOfFileExpected: return localize('error.endOfFileExpected', 'End of file expected');
 		default:
 			return '';
 	}
